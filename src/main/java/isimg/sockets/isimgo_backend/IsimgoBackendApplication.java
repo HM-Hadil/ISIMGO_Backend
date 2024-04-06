17a99831570fcd2922e83b8f6a3b7128a6dadf59
HM-Hadil <hadilhammami670@gmail.com>
2024-04-06 22:19:26 +0200

    initialize project websocket real time chat app

---
+++ src/main/java/isimg/sockets/isimgo_backend/IsimgoBackendApplication.java
@@ -0,0 +1,13 @@
+package isimg.sockets.isimgo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsimgoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsimgoBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/isimg/sockets/isimgo_backend/config/WebSocketConfig.java
@@ -0,0 +1,4 @@
+package isimg.sockets.isimgo_backend.config;
+
+public class WebSocketConfig {
+}
